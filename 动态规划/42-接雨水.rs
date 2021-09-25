c556c2cf3e9fcac0c91f36070e5cdf72ee41a679
KuangjuX <qcx@tju.edu.cn>
2021-09-24 19:21:22 -0700

    update

--- 动态规划/42-接雨水.rs
+++ 动态规划/42-接雨水.rs
@@ -8,6 +8,7 @@ impl Solution {
         max
     }
 
+    // 直接按列求
     pub fn trap(height: Vec<i32>) -> i32 {
         let mut sum = 0;
         for col in 1..height.len() - 1 {
@@ -18,6 +19,29 @@ impl Solution {
                 increase = 0;
             }
             
+            sum += increase;
+        }
+        sum
+    }
+
+    // 动态规划
+    pub fn trap2(height: Vec<i32>) -> i32 {
+        let mut sum = 0;
+        let mut left_max = vec![0;height.len()];
+        let mut right_max = vec![0;height.len()];
+        left_max[0] = height[0];
+        right_max[height.len() - 1] = height[height.len() - 1];
+        for col in 1..height.len() - 1 {
+            left_max[col] = cmp::max(left_max[col - 1], height[col - 1]);
+        }
+        for col in (1..height.len() - 1).rev() {
+            right_max[col] = cmp::max(right_max[col + 1], height[col + 1]);
+        }
+        for col in 1..height.len() - 1 {
+            let mut increase = cmp::min(left_max[col], right_max[col]) - height[col];
+            if increase < 0 {
+                increase = 0;
+            }
             sum += increase;
         }
         sum
